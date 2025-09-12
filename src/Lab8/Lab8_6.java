b24606363c4028112e6c89c35c3e3c6541b77bc9
Vino-Ari <ivannovoselskij495@gmail.com>
2025-09-12 23:47:27 +0300

    8 лаба

--- src/Lab8/Lab8_6.java
+++ src/Lab8/Lab8_6.java
@@ -1,12 +1,45 @@
 package Lab8;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 
 public class Lab8_6 {
     public static void main(String[] args) {
-
+        StringBuilder text = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\ivann\\IdeaProjects\\JavaLab\\src\\Lab8\\Lab8_6.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                text.append(line).append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Файл не найден.");
+            return;
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+            return;
+        }
+        String[] sentences = text.toString().split("[.?!]");
+        System.out.println("Введите ключевое слово:");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        scanner.close();
+        boolean foundAnySentence = false;
+        for (String sentence : sentences) {
+            if (sentence.isEmpty()) {
+                continue;
+            }
+            if (sentence.matches(".*(?i)\\b" + Pattern.quote(word) + "\\b.*")) {//выучить
+                System.out.println(sentence);
+                foundAnySentence = true;
+            }
+        }
+        if (!foundAnySentence) {
+            System.out.println("Ни одно предложение не содержит указанное ключевое слово.");
+        }
     }
-
 }
--- src/Lab8/Lab8_7.java
+++ src/Lab8/Lab8_7.java
@@ -1,4 +1,33 @@
 package Lab8;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Lab8_7 {
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\ivann\\IdeaProjects\\JavaLab\\src\\Lab8\\Lab8_6.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                text.append(line).append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Файл не найден.");
+            return;
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+            return;
+        }
+        int price = 0;
+        text.replace(0, text.length(), text.toString().replace(",", " zpt "));
+        String[] words = text.toString().split(" ");
+        for (String word : words) {
+            if (word.length() <= 2) continue;
+            price += 10;
+            System.out.print(word + " ");
+        }
+        System.out.printf("Стоимость телеграммы составляет: %dр", price);
+    }
 }
